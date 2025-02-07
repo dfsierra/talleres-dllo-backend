de597c8747830dcabb447d4e34a42a161327f13c
Daniel Felipe Sierra <98929198+dfsierra@users.noreply.github.com>
2025-02-07 18:32:54 -0500

    Update punto2.js

--- Taller1/punto2.js
+++ Taller1/punto2.js
@@ -1,1 +1,17 @@
 
+function resolvedor(a,b,c, positivo) {
+	const disc = Math.pow(b,2)-4*a*c
+  if (disc<0) {
+  	return "deja el invento"
+  }
+  const raiz = Math.sqrt(disc)
+  if (positivo) {
+  return ((-b + raiz)/(2*a) )
+  } else {
+  return ((-b - raiz)/(2*a) )
+  }
+}
+
+const a=1,b=5,c=4
+console.log("Positivo: "+ resolvedor(a,b,c, true))
+console.log("Negativo: "+ resolvedor(a,b,c, false))
